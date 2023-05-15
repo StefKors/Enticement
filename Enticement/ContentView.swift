9f12351538d51eaba7186fbdad6064e585ed30c8
StefKors <stefkors@Stefs-MacBook-Pro.local>
2023-05-15 15:58:16 +0200

    layout improvements

--- Enticement/ContentView.swift
+++ Enticement/ContentView.swift
@@ -15,76 +15,70 @@ struct CheckItemView: View {
 
     var body: some View {
         HStack {
-            Button {
-                withAnimation(.spring()) {
-                    print("tap!")
-                    item.setValue(!item.isCompleted, forKey: "isCompleted")
-                    do {
-                        try viewContext.save()
-                    } catch {
-                        // Replace this implementation with code to handle the error appropriately.
-                        // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                        let nsError = error as NSError
-                        fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-                    }
-                }
-            } label: {
-                Label {
-                    Text(item.text!)
-                } icon: {
-                    ZStack {
-                        if item.isCompleted {
-                            Circle().fill()
-                                .transition(.scale.combined(with: .opacity))
-                        } else {
-                            Circle().stroke()
-                                .transition(.scale.combined(with: .opacity))
-                            Circle().fill().opacity(0.3)
-                                .transition(.scale.combined(with: .opacity))
+            if let text = item.text {
+                Button {
+                    handleTap()
+                } label: {
+                    Label {
+                        // Text(text)
+                    } icon: {
+                        ZStack {
+                            if item.isCompleted {
+                                Circle().fill()
+                                    .transition(.scale.combined(with: .opacity))
+                            } else {
+                                Circle().stroke()
+                                    .transition(.scale.combined(with: .opacity))
+                                Circle().fill().opacity(0.3)
+                                    .transition(.scale.combined(with: .opacity))
+                            }
                         }
+                        .clipShape(Circle())
                     }
-                    .clipShape(Circle())
                 }
+                .buttonStyle(.plain)
+
+                Text(text)
+                // Spacer()
+                // Button(role: .destructive) {
+                //     withAnimation(.spring()) {
+                //         viewContext.delete(item)
+                //     }
+                // } label: {
+                //     Label("Delete", systemImage: "trash")
+                // }
+                // .buttonStyle(.plain)
+                // .labelStyle(.iconOnly)
             }
-            .buttonStyle(.plain)
-            Spacer()
-            Button(role: .destructive) {
-                withAnimation(.spring()) {
-                    viewContext.delete(item)
-                }
-            } label: {
-                Label("Delete", systemImage: "trash")
+        }
+    }
+
+    func handleTap() {
+        withAnimation(.spring()) {
+            print("tap!")
+            item.setValue(!item.isCompleted, forKey: "isCompleted")
+            do {
+                try viewContext.save()
+            } catch {
+                // Replace this implementation with code to handle the error appropriately.
+                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
-            .buttonStyle(.plain)
-            .labelStyle(.iconOnly)
         }
     }
 }
 
-struct ContentView: View {
+struct TextFieldView: View {
     @Environment(\.managedObjectContext) private var viewContext
 
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
     @State private var editorContent: String = ""
-
     var body: some View {
-        List {
-            TextField("Todo Input", text: $editorContent, prompt: Text("Write here..."))
-                .onSubmit {
-                    addItem(label: editorContent)
-                    editorContent = ""
-                }
-            ForEach(items) { item in
-                HStack {
-                    CheckItemView(item: item)
-                }
+        TextField("Todo Input", text: $editorContent, prompt: Text("Write here..."))
+            .onSubmit {
+                addItem(label: editorContent)
+                editorContent = ""
             }
-            .onDelete(perform: deleteItems)
-        }
     }
 
     private func addItem(label: String) {
@@ -101,6 +95,30 @@ struct ContentView: View {
                 let nsError = error as NSError
                 fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
             }
+        }
+    }
+}
+
+struct ContentView: View {
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
+        animation: .default)
+    private var items: FetchedResults<Item>
+
+
+
+    var body: some View {
+        List {
+            TextFieldView()
+
+            ForEach(items) { item in
+                HStack {
+                    CheckItemView(item: item)
+                }
+            }
+            .onDelete(perform: deleteItems)
         }
     }
 
